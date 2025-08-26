37f0e583b32542fdb2047529cf00e7d41b823947
unknown <jonathan.kuckert@rwth-aachen.de>
2025-08-26 11:57:45 +0200

    changing branch

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.core.files.storage import FileSystemStorage
 # Create your models here.
+
+class UserFiles(models.Model):
+    username =models.CharField(max_length=20)
+    fs = FileSystemStorage(location='/'+str(username)+'/files')
+    file = models.FileField(storage=fs)
+
+    
