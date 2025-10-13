26561770dccedc087a0bd0334e0f2df7c61b79d4
Toan Nguyen <toan.nguyen@rwth-aachen.de>
2025-10-13 10:58:17 +0200

    generate figure

--- totem_lib/example_ocpn.py
+++ totem_lib/example_ocpn.py
@@ -3,4 +3,6 @@ import os
 
 ocel = pm4py.read_ocel2_json(os.path.join("example_data", "ContainerLogistics.json"))
 model = pm4py.discover_oc_petri_net(ocel)
-pm4py.view_ocpn(model, format="png")
+output_path = os.path.join("figures", "ContainerLogistics_ocpn_pm4py.png")
+# pm4py.view_ocpn(model, format="png")
+pm4py.save_vis_ocpn(model, output_path)
