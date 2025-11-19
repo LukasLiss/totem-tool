be8bdf34c9ba7a893dd2bf0114781e6c84f39ea9
Toan Nguyen <toan.nguyen@rwth-aachen.de>
2025-11-19 10:48:58 +0100

    implement health-check api

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,13 +2,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
-from .views import EventLogViewSet, greeting, variants, DashboardViewSet
+from .views import EventLogViewSet, greeting, variants, DashboardViewSet, health_check
 
 router = DefaultRouter()
 router.register(r'files', EventLogViewSet, basename="userfile")
 router.register(r'dashboard', DashboardViewSet, basename="dashboard")
 
 urlpatterns = [
+    path('health-check/', health_check, name='health-check'),
     path('greeting/', greeting, name='greeting'),
     path("", include(router.urls)),
     path("variants/", variants, name="variants"),
