41cd56d4441f1e63f4bc3d61c00f7412b8f3f3bc
Lukas Liss <33811077+LukasLiss@users.noreply.github.com>
2026-02-03 10:45:05 +0100

    title page spelling

--- frontend/src/Title.tsx
+++ frontend/src/Title.tsx
@@ -4,7 +4,7 @@ export function Title() {
   return (
     <div className="flex flex-col items-center justify-center h-dvh gap-8">
         <h1 className="text-4xl font-bold mb-8 text-center">
-            TOTem - Process Mining Tool
+            TOTeM - Process Mining Tool
         </h1>
         <Button variant="default" className="text-center" onClick={ () => { window.location.href = '/login'; } }>
             Log in
