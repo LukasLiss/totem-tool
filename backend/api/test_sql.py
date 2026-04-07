611e89e26fbaac9ad2670c83016982cbb6f7b666
Jonathan Kuckert <134513772+JonathanKuckert@users.noreply.github.com>
2026-04-07 23:15:21 +0200

    First tests

--- backend/api/test_sql.py
+++ backend/api/test_sql.py
@@ -20,3 +20,4 @@ duckdb.register('objects', data.objects.to_pandas())
 print(duckdb.sql('SELECT * FROM objects'))
 
 print(duckdb.sql("""SELECT e._activity, e._objects, e._qualifiers FROM events e WHERE CAST(e._objects AS VARCHAR) LIKE '%Benedikt Knopp%'"""))
+
