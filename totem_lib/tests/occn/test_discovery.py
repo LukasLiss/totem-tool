a4bd02a02188db583e885fc44dab5151bfd2579c
Vasu021 <amlendushekhar828@gmail.com>
2026-06-30 15:10:43 +0200

    Remove unnecessary sys.path from test_discovery

--- totem_lib/tests/occn/test_discovery.py
+++ totem_lib/tests/occn/test_discovery.py
@@ -1,7 +1,4 @@
-import sys
 from pathlib import Path
-
-sys.path.insert(0, str(Path(__file__).parent.parent.parent / "src"))
 
 from totem_lib import import_ocel, discover_occn
 from totem_lib.ocel import schema_base_filtering, propagate_filtering
