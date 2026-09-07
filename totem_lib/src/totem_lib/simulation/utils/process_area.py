93dadd82fd9f8f76d290b0dc565d21a914e74830
Bastian <bastian.levartz@twth-aachen.de>
2026-09-07 20:54:08 +0200

    Removed unnecessary attribute from class ProcessArea

--- totem_lib/src/totem_lib/simulation/utils/process_area.py
+++ totem_lib/src/totem_lib/simulation/utils/process_area.py
@@ -1,26 +1,24 @@
 class ProcessArea:
     """
     Represents a single process area consisting of a set of activities, the
-    business object types the process runs on, the object types acting as
-    resources, and a level.
-S """
+    business object types the process runs on, and the object types acting as
+    resources.
+    """
 
     def __init__(
         self,
         object_types: list[str],
         activities: list[str],
-        level: float = None,
         resource_types: list[str] = None,
     ):
         self.object_types = list(object_types)
         self.activities = list(activities)
-        self.level = level
         self.resource_types = list(resource_types) if resource_types else []
 
     def __repr__(self):
         return (
-            f"<ProcessArea level={self.level}, "
-            f"object_types={self.object_types}, "
+            f"<ProcessArea object_types={self.object_types}, "
             f"resource_types={self.resource_types}, "
-            f"activities={[a for a in self.activities[:3]]}{'...' if len(self.activities) > 3 else ''}>"
+            f"activities={[a for a in self.activities[:3]]}"
+            f"{'...' if len(self.activities) > 3 else ''}>"
         )
